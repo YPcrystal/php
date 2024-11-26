aa9c25383e3e38051d1b6ad5f70a9eed324b3cc6
YPcrystal <azzahroshoffyah@gmail.com>
2024-11-26 09:33:41 +0700

    hapus

--- tugas2a.php
+++ tugas2a.php
@@ -1,5 +1,6 @@
 <?php
 
+//perkalian
 for(
    $i = 1;
    $i<= 10;
@@ -10,10 +11,68 @@ for(
    $j++
  ){
    echo $i * $j . "\t";
-   //echo $i / $j . "\t";
-   //echo $i ** $j. "\t";
-   //echo $i + $j . "\t";
-   //echo $i - $j . "\t";
  }
  echo "\n";
+}
+
+//pembagian
+for(
+  $i = 1;
+  $i<= 10;
+  $i++
+){for(
+  $j = 1;
+  $j <= 10;
+  $j++
+){
+  echo $i / $j . "\t";
+}
+echo "\n";
+}
+
+// pangkat
+for(
+  $i = 1;
+  $i<= 10;
+  $i++
+){for(
+  $j = 1;
+  $j <= 10;
+  $j++
+){
+  echo $i ** $j. "\t";
+}
+echo "\n";
+}
+
+//penjumlahan
+
+for(
+  $i = 1;
+  $i<= 10;
+  $i++
+){for(
+  $j = 1;
+  $j <= 10;
+  $j++
+){
+  echo $i + $j . "\t";
+}
+echo "\n";
+}
+
+//pengurangan
+
+for(
+  $i = 1;
+  $i<= 10;
+  $i++
+){for(
+  $j = 1;
+  $j <= 10;
+  $j++
+){
+  echo $i - $j . "\t";
+}
+echo "\n";
 }
