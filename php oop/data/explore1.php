86efb43d59040873ced95aeef0442975b4840ae7
YPcrystal <azzahroshoffyah@gmail.com>
2024-12-04 00:49:23 +0700

    bismillah

--- php oop/data/explore1.php
+++ php oop/data/explore1.php
@@ -12,3 +12,7 @@ class MyClass {
 
 $obj = new MyClass(); // Constructor dipanggil
 unset($obj); // Destructor dipanggil
+
+// Menutup koneksi database yang terbuka.
+// Menghapus file atau resource lain yang sudah tidak diperlukan.
+// Membersihkan memori jika objek menggunakan resource yang mahal seperti file atau jaringan.
