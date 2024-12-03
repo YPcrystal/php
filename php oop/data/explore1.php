19cd714b945cbc52bdfcd848e044d83662cf7ffc
YPcrystal <azzahroshoffyah@gmail.com>
2024-12-04 00:39:32 +0700

    bismillah

--- php oop/data/explore1.php
+++ php oop/data/explore1.php
@@ -1,1 +1,14 @@
 <?php
+
+class MyClass {
+    function __construct() {
+        echo "Objek telah dibuat.<br>";
+    }
+    
+    function __destruct() {
+        echo "Objek telah dihancurkan.<br>";
+    }
+}
+
+$obj = new MyClass(); // Constructor dipanggil
+unset($obj); // Destructor dipanggil
