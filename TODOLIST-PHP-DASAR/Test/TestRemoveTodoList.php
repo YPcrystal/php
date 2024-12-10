7eabee6898c9ad6ed1d829fec4604d0d15a65af0
YPcrystal <azzahroshoffyah@gmail.com>
2024-12-10 15:58:32 +0700

    bismillah

---
+++ TODOLIST-PHP-DASAR/Test/TestRemoveTodoList.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
+addTodoList("Belajar PHP");
+addTodoList("Belajar PHP OOP");
+addTodoList("Belajar PHP Web");
+addTodoList("Belajar Mysql");
+addTodoList("Belajar Laravel");
+
+showTodoList();
+
+removeTodoList();
+
+showTodoList();
+
+removeTodoList();
+
+showTodoList();
+
+$succes = removeTodoList(4);
+var_dump($succes);
+
+showTodoList();
