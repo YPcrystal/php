ed2dd482d1efb69f5eb54d735042e4fffed0f086
YPcrystal <azzahroshoffyah@gmail.com>
2024-12-16 21:50:25 +0700

    helper

--- TODOLIST-PHP-OOP/Helper/InputHelper.php
+++ TODOLIST-PHP-OOP/Helper/InputHelper.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace Helper {
-    class InputHelper {
-        static function input(string $info): string {
+
+    class InputHelper
+    {
+        static function input(string $info): string
+        {
             echo "$info : ";
             $result = fgets(STDIN);
             return trim($result);
         }
     }
+
 }
